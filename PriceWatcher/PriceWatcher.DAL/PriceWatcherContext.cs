e5a9927133937a3d334eb041f0cfd257902109f1
Cristian Santilli <csantilli@southplans.com.ar>
2016-07-07 17:31:02 -0300

    Adding models.

---
+++ PriceWatcher/PriceWatcher.DAL/PriceWatcherContext.cs
@@ -0,0 +1,26 @@
+﻿using PriceWatcher.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceWatcher.DAL
+{
+    public class PriceWatcherContext : DbContext
+    {
+        public PriceWatcherContext() : base("PriceWatcherContext")
+        { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Price> Prices { get; set; }
+        public DbSet<Retailer> Retailers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ PriceWatcher/PriceWatcher.DAL/PriceWatcherInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceWatcher.DAL
+{
+    public class PriceWatcherInitializer : DropCreateDatabaseIfModelChanges<PriceWatcherContext>
+    {
+        protected override void Seed(PriceWatcherContext context)
+        {
+            context.Products.Add(new Models.Product() { Name = "Roomba" });
+            context.Products.Add(new Models.Product() { Name = "Iphone" });
+
+            context.SaveChanges();
+        }
+    }
+}
