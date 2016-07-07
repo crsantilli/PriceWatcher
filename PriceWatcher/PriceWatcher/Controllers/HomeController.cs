2b3813b4048002eea39dc2cc4cfb5e5fc171fc53
Cristian Santilli <csantilli@southplans.com.ar>
2016-07-07 11:48:09 -0300

    Added highcharts.js and getting values from services.

--- PriceWatcher/PriceWatcher/Controllers/HomeController.cs
+++ PriceWatcher/PriceWatcher/Controllers/HomeController.cs
@@ -20,11 +20,22 @@ namespace PriceWatcher.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult GetValues()
         {
-            ViewBag.Message = "Your contact page.";
+            List<Country> countries = new List<Country>();
 
-            return View();
+            countries.Add(new Country() { name = "Tokyo", data = new List<decimal> { 7.0m, 6.9m, 9.5m, 14.5m, 18.2m, 21.5m, 25.2m, 26.5m, 23.3m, 18.3m, 13.9m, 9.6m } });
+            countries.Add(new Country() { name = "New York", data = new List<decimal> { -0.2m, 0.8m, 5.7m, 11.3m, 17.0m, 22.0m, 24.8m, 24.1m, 20.1m, 14.1m, 8.6m, 2.5m } });
+            countries.Add(new Country() { name = "Berlin", data = new List<decimal> { -0.9m, 0.6m, 3.5m, 8.4m, 13.5m, 17.0m, 18.6m, 17.9m, 14.3m, 9.0m, 3.9m, 1.0m } });
+            countries.Add(new Country() { name = "London", data = new List<decimal> { 3.9m, 4.2m, 5.7m, 8.5m, 11.9m, 15.2m, 17.0m, 16.6m, 14.2m, 10.3m, 6.6m, 4.8m} });
+
+            return Json(new { countries = countries }, JsonRequestBehavior.AllowGet);
+        }
+
+        public class Country
+        {
+            public string name { get; set; }
+            public List<decimal> data { get; set; }
         }
     }
 }
--- PriceWatcher/PriceWatcher/Scripts/angularLogic/heroDetail.js
+++ PriceWatcher/PriceWatcher/Scripts/angularLogic/heroDetail.js
@@ -1,7 +1,54 @@
 ﻿(function (angular) {
     'use strict';
-    function HeroDetailController() {
+    function HeroDetailController($http) {
+        var ctrl = this;
 
+        ctrl.$onInit = function () {
+
+            $http({
+                method: 'GET',
+                url: '/Home/GetValues'
+            }).then(function successCallback(response) {
+
+                $('#container').highcharts({
+                    title: {
+                        text: 'Monthly Average Temperature',
+                        x: -20 //center
+                    },
+                    subtitle: {
+                        text: 'Source: WorldClimate.com',
+                        x: -20
+                    },
+                    xAxis: {
+                        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Temperature (°C)'
+                        },
+                        plotLines: [{
+                            value: 0,
+                            width: 1,
+                            color: '#808080'
+                        }]
+                    },
+                    tooltip: {
+                        valueSuffix: '°C'
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: response.data.countries
+                });
+
+                
+            }, function errorCallback(response) {
+                alert("Sorry, something went wrong!");
+            });            
+        }
     }
 
     angular.module('heroApp').component('heroDetail', {
